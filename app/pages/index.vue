ddb4a751293b49b4cf4763b37fc01020072b9e64
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-09-28 16:30:19 -0400

    yes sir

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="main min-h-screen flex flex-col items-center justify-center bg-black text-white gap-4">
-      <h1 class="text-7xl">Magnets and ElectroMagnets</h1>
-      <img class="border border-amber-400 p-6 cursor-pointer hover:opacity-50 hover:rounded-3xl transiiton transition-all duration-100" src="/magnet.png"></img>
+      <h1 class="md:text-7xl text-5xl md:w-fit w-3/4">Magnets and ElectroMagnets</h1>
+      <img class="border border-amber-400 p-6 cursor-pointer hover:opacity-50 hover:rounded-3xl transiiton transition-all duration-300" src="/magnet.png"></img>
   </div>
 </template>
