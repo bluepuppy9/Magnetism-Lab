e8ce5e321840f012e17882c2bfb25e08f3db4459
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-09-28 16:26:10 -0400

    frontend good

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app" class="main min-h-screen flex flex-col items-center justify-center bg-black text-white">
-    <h1 class="text-7xl">Magnets and ElectroMagnets</h1>
+  <div id="app" class="main min-h-screen flex flex-col items-center justify-center bg-black text-white gap-4">
+      <h1 class="text-7xl">Magnets and ElectroMagnets</h1>
+      <img class="border border-amber-400 p-6 cursor-pointer hover:opacity-50 hover:rounded-3xl transiiton transition-all duration-100" src="/magnet.png"></img>
   </div>
 </template>
